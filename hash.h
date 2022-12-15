610bf59b7d0fcbc0fa55fae3f51821966c67d1cc
Michael/Misha Khmelkov <khmelkov@usc.edu>
2022-12-14 16:39:53 -0800

    String hash implemented

--- hash.h
+++ hash.h
@@ -20,7 +20,22 @@ struct MyStringHash {
     HASH_INDEX_T operator()(const std::string& k) const
     {
         // Add your code here
+        unsigned long long w[5] = {0, 0, 0, 0, 0};
 
+        // Fill w array
+        int x = k.size() - 1;
+        for (int i = 4; i>=0; i--) {
+            for (int j = 0; j<6; j++) {
+                if (x >= 0) {
+                    w[i] += letterDigitToNumber(k[x]) * pow(36, j);
+                }
+                x--;
+            }
+        }
+
+        HASH_INDEX_T sum = 0;
+        for (int i = 0; i<5; i++) sum += rValues[i] * w[i];
+        return sum;
 
     }
 
@@ -28,7 +43,11 @@ struct MyStringHash {
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
         // Add code here or delete this helper function if you do not want it
-
+        letter = tolower(letter);
+        // a-z
+        if (letter >= 97) return letter - 97;
+        // 0-9
+        else return letter - 48 + 26;
     }
 
     // Code to generate the random R values
